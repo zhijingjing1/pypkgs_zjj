d60dcc326b4f415bdcae94a79dd8287dd30ca49c
lenovo <jingjingzhi66@163.com>
2021-02-25 19:35:50 +0800

    added unit tests for catbind

--- pypkgs_zjj/pypkgs_zjj.py
+++ pypkgs_zjj/pypkgs_zjj.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def catbind(a, b):
-     """
+    """
     Concatenates two pandas categoricals.
 
     Parameters
@@ -19,10 +19,10 @@ def catbind(a, b):
 
     Examples
     --------
-    >>> from pypkgs import pypkgs
+    >>> from pypkgs_zjj import pypkgs_zjj
     >>> a = pd.Categorical(["character", "hits", "your", "eyeballs"])
     >>> b = pd.Categorical(["but", "integer", "where it", "counts"])
-    >>> pypkgs.catbind(a, b)
+    >>> pypkgs_zjj.catbind(a, b)
     [character, hits, your, eyeballs, but, integer, where it, counts]
     Categories (8, object): [but, character, counts,
     eyeballs, hits, integer, where it, your]
--- tests/test_pypkgs_zjj.py
+++ tests/test_pypkgs_zjj.py
@@ -1,5 +1,13 @@
 from pypkgs_zjj import __version__
 from pypkgs_zjj import pypkgs_zjj
+import pandas as pd
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    assert ((pypkgs_zjj.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+    assert ((pypkgs_zjj.catbind(a, b)).categories == ["but", "character",
+            "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
